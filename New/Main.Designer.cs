dbf34c0d6e06fe42a315f19c4b3b82eba2d3ae1d
TuanTin <tuan93hvqs@gmail.com>
2017-10-20 10:30:59 +0700

    Update project New

---
+++ New/Main.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace New
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lbListRow = new System.Windows.Forms.ListBox();
+            this.lbListRel = new System.Windows.Forms.ListBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(544, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(544, 425);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtname
+            // 
+            this.txtname.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtname.Location = new System.Drawing.Point(13, 13);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(525, 20);
+            this.txtname.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lbListRow
+            // 
+            this.lbListRow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbListRow.FormattingEnabled = true;
+            this.lbListRow.Location = new System.Drawing.Point(13, 41);
+            this.lbListRow.Name = "lbListRow";
+            this.lbListRow.Size = new System.Drawing.Size(295, 186);
+            this.lbListRow.TabIndex = 4;
+            // 
+            // lbListRel
+            // 
+            this.lbListRel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbListRel.FormattingEnabled = true;
+            this.lbListRel.Location = new System.Drawing.Point(314, 41);
+            this.lbListRel.Name = "lbListRel";
+            this.lbListRel.Size = new System.Drawing.Size(305, 186);
+            this.lbListRel.TabIndex = 5;
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Location = new System.Drawing.Point(13, 233);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(606, 186);
+            this.listView.TabIndex = 6;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.lbListRel);
+            this.Controls.Add(this.lbListRow);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Main";
+            this.Text = "Read File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox lbListRow;
+        private System.Windows.Forms.ListBox lbListRel;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ New/Main.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace New
+{
+    public partial class Main : Form
+    {
+        string _filename = "";
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public int ReadFileToBase(string path_File)
+        {
+            int rel = 0;
+            try
+            {
+                int recordType = 0;
+                string record = "";
+                string btranID = "";
+                //Path file
+                string[] lines = System.IO.File.ReadAllLines(path_File);
+                /*
+                 * Read file
+                 * Record type:
+                 *   0 : file header - no
+                 *   1 : batch header - yes
+                 *   2 : trasaction detail - yes
+                 *   7 : batch trailer - yes
+                 *   9 : file trailer - yes
+                 */
+                int irecord = 0;
+                foreach (string iline in lines)
+                {
+                    lbListRow.Items.Add(iline);
+
+                    recordType = int.Parse(iline.Substring(0,1));
+                    if (recordType != 0 && recordType != 9)
+                    {
+                        
+                        irecord = irecord + 1;
+                        string tmp = "00000000" + irecord.ToString();
+                        btranID = "BID" + tmp.PadRight(8);
+                        record = iline.Substring(38,iline.Length - 38).ToString();
+                        lbListRel.Items.Add(record);
+
+                        //
+                        // Add subitem
+                        ListViewItem Items = new ListViewItem();
+                        Items.Text = btranID;
+                        Items.SubItems.Add(record.Substring(0, 10));
+                        int iCR = record.IndexOf("CR");
+                        int iDR = record.IndexOf("DR");
+                        if ( iCR > 0)
+                        {
+                            Items.SubItems.Add(record.Substring(10, iCR - 10));
+                            Items.SubItems.Add(record.Substring(iCR, 2));
+                            if (iCR + 2 != record.Length - 1)
+                            {
+                                Items.SubItems.Add(record.Substring(iCR + 2, 2));
+                                Items.SubItems.Add(record.Substring(iCR + 4, record.Length - iCR -5));
+                            }
+                            else
+                            {
+                                Items.SubItems.Add("");
+                                Items.SubItems.Add("");
+                                
+                            }
+                            Items.SubItems.Add(DateTime.Now.ToShortDateString());
+                        }
+                        else if ( iDR> 0)
+                        {
+                            Items.SubItems.Add(record.Substring(10, iDR - 10));
+                            Items.SubItems.Add(record.Substring(iDR , 2));
+                            if (iDR + 2 != record.Length - 1)
+                            {
+                                Items.SubItems.Add(record.Substring(iDR + 2, 2));
+                                Items.SubItems.Add(record.Substring(iDR + 4, record.Length - iDR - 5));
+                            }
+                            else
+                            {
+                                Items.SubItems.Add("");
+                                Items.SubItems.Add("");
+                                
+
+                            }
+                            Items.SubItems.Add(DateTime.Now.ToShortDateString());
+
+                        }
+                        
+                        listView.Items.Add(Items);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return rel = -1;
+                throw new Exception(ex.Message);
+            }
+            return rel;
+        }
+
+        private void CreateListView()
+        {
+            listView.Columns.Add("btranID", 100);
+            listView.Columns.Add("btranDate", 100);
+            listView.Columns.Add("btranAmount", 100);
+            listView.Columns.Add("btranCRDR", 100);
+            listView.Columns.Add("btranTrCode", 100);
+            listView.Columns.Add("btranTrDescRef",100);
+            listView.Columns.Add("btranCreated", 100);
+
+            listView.View = View.Details;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string _filename = "";
+            openFileDialog1.FileName = "";
+
+            CreateListView();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _filename = openFileDialog1.FileName;
+                txtname.Text = _filename;
+                ReadFileToBase(_filename);
+            }
+        }
+
+        
+    }
+}
